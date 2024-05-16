945b2062ede71f373f0e27da97054f3685dd2d7a
Garrett <Garrett@YUP>
2024-05-16 11:23:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Schema;
+
+namespace Challenge._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string storedUserName = "Armani";
+            const string storedPassWord = "Password";
+            bool success = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter your username.");
+                string username = Console.ReadLine();
+                Console.WriteLine("Enter your password.");
+                string password = Console.ReadLine();
+
+                if (username == storedUserName && password == storedPassWord)
+                {
+                    success = true;
+                    break;
+                }
+                Console.WriteLine();
+            }
+
+            if (success)
+            {
+                Console.WriteLine("Welcome " + storedUserName);
+            }
+            else
+            {
+                Console.WriteLine("Account blocked");
+            }
+           
+        }
+        
+    }
+}
